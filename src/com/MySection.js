5b268baa178fe2813cf3946e5492091868ed222a
danmiee <dan-mi@kakao.com>
2022-10-12 15:09:25 +0900

    오타수정

--- src/com/MySection.js
+++ src/com/MySection.js
@@ -7,17 +7,17 @@ import imgcss from '../img/css.png';
 import imgjs from '../img/js.png';
 import imgreact from '../img/react.png';
 
-// 매개변수는 변경 가능하나 소통을 위해 probs로 많이 사용함
-function MySection(probs) {
-  console.log(probs.h1);
+// 매개변수는 변경 가능하나 소통을 위해 props로 많이 사용함
+function MySection(props) {
+  console.log(props.h1);
   let imgName = null;
 
-  if (probs.h1 === 'HTML') imgName = imghtml;
-  else if (probs.h1 === 'CSS') imgName = imgcss;
-  else if (probs.h1 === 'JS') imgName = imgjs;
-  else if (probs.h1 === 'React') imgName = imgreact;
+  if (props.h1 === 'HTML') imgName = imghtml;
+  else if (props.h1 === 'CSS') imgName = imgcss;
+  else if (props.h1 === 'JS') imgName = imgjs;
+  else if (props.h1 === 'React') imgName = imgreact;
 
-  /* probs에 따라 object로 구분 */
+  /* props에 따라 object로 구분 */
   const pContent = {
     'HTML': 'HTML(HyperText Markup Language)은 웹을 이루는 가장 기초적인 구성 요소로, 웹 콘텐츠의 의미와 구조를 정의할 때 사용',
     'CSS': 'Cascading Style Sheets(CSS)는 HTML이나 XML(XML의 방언인 SVG, XHTML 포함)로 작성된 문서의 표시 방법을 기술하기 위한 스타일 시트 언어',
@@ -28,11 +28,11 @@ function MySection(probs) {
   return (
     <section>
       <div>
-        <img src={imgName} alt={probs.h1} />
+        <img src={imgName} alt={props.h1} />
       </div>
-      <h1 className="content">{probs.h1}</h1>
+      <h1 className="content">{props.h1}</h1>
       {/* key값 가져올 때는 [] 사용 */}
-      <div><p>{pContent[probs.h1]}</p></div>
+      <div><p>{pContent[props.h1]}</p></div>
     </section>
   );
 }
